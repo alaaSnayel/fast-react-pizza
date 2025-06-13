7752eb1bac44e94b261bb5bacf17e09500a68b41
alaaSnayel <alaanayel111@gmail.com>
2025-06-13 10:15:06 +0300

    Reading and updating the user state

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -1,6 +1,7 @@
 import { Form, redirect, useActionData, useNavigate } from "react-router-dom";
 import { createOrder } from "../../services/apiRestaurant";
 import Button from "../../ui/Button";
+import { useSelector } from "react-redux";
 
 // https://uibakery.io/regex-library/phone-number
 const isValidPhone = (str) =>
@@ -40,6 +41,7 @@ function CreateOrder() {
 
   // const [withPriority, setWithPriority] = useState(false);
   const cart = fakeCart;
+  const userName = useSelector((state) => state.user.userName);
 
   return (
     <div className="px-4 py-6">
@@ -49,7 +51,13 @@ function CreateOrder() {
       <Form method="POST">
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
           <label className="sm:basis-40">First Name</label>
-          <input className="input grow" type="text" name="customer" required />
+          <input
+            className="input grow"
+            type="text"
+            name="customer"
+            defaultValue={userName}
+            required
+          />
         </div>
 
         <div className="mb-5 flex flex-col gap-2 sm:flex-row sm:items-center">
--- src/ui/Home.jsx
+++ src/ui/Home.jsx
@@ -1,17 +1,27 @@
-import CreateUser from "../features/user/CreateUser"
+import { useSelector } from "react-redux";
+import CreateUser from "../features/user/CreateUser";
+import Button from "./Button";
 
 function Home() {
+  const userName = useSelector((state) => state.user.userName);
+
   return (
-    <div className='text-center my-10 sm:my-16 px-4'>
-      <h1 className='text-xl font-semibold mb-8 md:text-3xl'>
+    <div className="text-center my-10 sm:my-16 px-4">
+      <h1 className="text-xl font-semibold mb-8 md:text-3xl">
         The best pizza.
         <br />
-        <span className='text-yellow-500'>
-        Straight out of the oven, straight to you.
+        <span className="text-yellow-500">
+          Straight out of the oven, straight to you.
         </span>
       </h1>
 
-      <CreateUser />
+      {userName === "" ? (
+        <CreateUser />
+      ) : (
+        <Button to="/menu" type="primary">
+          Continue ordering, {userName}
+        </Button>
+      )}
     </div>
   );
 }
