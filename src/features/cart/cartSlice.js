5fdcf6e4786cc36e8f94e0b9e15904c4dd5e9e47
alaaSnayel <alaanayel111@gmail.com>
2025-06-18 11:06:59 +0300

    Deleting cart item

--- src/features/cart/cartSlice.js
+++ src/features/cart/cartSlice.js
@@ -65,3 +65,9 @@ export const getTotalCartQuantity = (state) =>
 
 export const getTotalCartPrice = (state) =>
   state.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
+
+export const getCurrentQuantityById = (id) => (state) => {
+  const item =
+    state.cart.cart.find((item) => item.pizzaId === id)?.quantity ?? 0;
+  return item;
+};
