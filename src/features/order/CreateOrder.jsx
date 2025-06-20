bec287125e47b64f3f9a8d04b2b82f0734c26511
alaaSnayel <alaanayel111@gmail.com>
2025-06-20 11:08:13 +0300

    Updating Quantities in cart and menu

--- src/features/order/CreateOrder.jsx
+++ src/features/order/CreateOrder.jsx
@@ -112,7 +112,7 @@ function CreateOrder() {
 export async function action({ request }) {
   const formData = await request.formData();
   const data = Object.fromEntries(formData);
-  console.log(data);
+  // console.log(data);
 
   const order = {
     ...data,
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -10,6 +10,7 @@ function Button({ children, disabled, to, type, onClick }) {
     secondary:
       "px-4 py-2.5 text-sm md:px-6 md:py-3.5 border-2 hover:text-stone-800 focus:text-stone-800 border-stone-300 inline-block font-semibold uppercase text-stone-400 tracking-wide rounded-full hover:bg-stone-300 transition-colors duration-300 focus:outline-none focus:ring focus:ring-stone-300 focus:bg-stone-300 focus:ring-offset-2 first-line:disabled:cursor-not-allowed",
     small: base + " px-4 py-2 md:px-5 md:py-2.5 text-xs",
+    round: base + " px-2.5 py-1 md:px-3.5 md:py-2 text-sm",
   };
 
   if (to)
